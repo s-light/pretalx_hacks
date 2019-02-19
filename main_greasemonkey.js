e37d20c2562bc00d5ba2250804863606bec74b3b
s-light <github@s-light.eu>
2019-02-19 16:48:47 +0100

    some more css fixes

--- main_greasemonkey.js
+++ main_greasemonkey.js
@@ -2,10 +2,11 @@
 // @name     pretalx_hacks
 // @description some UI only hacks for pretalx
 // @namespace   https://github.com/s-light
-// @include     http://localhost:8000/*/schedule/*
+// @include     https://yourDomain/orga/event/*/schedule/
+// @include     https://yourDomain/*/schedule/*
 // @version  0.1.0
-// @require https://s-light.github.io/pretalx_hacks/helper.js
-// @require https://s-light.github.io/pretalx_hacks/helper_ui.js
+// require https://s-light.github.io/pretalx_hacks/helper.js
+// require https://s-light.github.io/pretalx_hacks/helper_ui.js
 // @require https://s-light.github.io/pretalx_hacks/fix_css.js
 // require https://s-light.github.io/pretalx_hacks/csv_exporter.js
 //
@@ -15,8 +16,9 @@
 function start_script() {
     console.info('pretalx_hacks..');
 
-    helper();
-    helper_ui();
+    //helper();
+    //helper_ui();
+  	fix_css();
 
     console.info('all user scripting done.');
 }
